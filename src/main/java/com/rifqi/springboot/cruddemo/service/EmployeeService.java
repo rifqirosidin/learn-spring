2f2ee22d4e2e20088a7d89b7ed082bd965427efa
Rifqi Rosidin <rifqi.rosidin@gmail.com>
2021-01-16 14:31:24 +0700

    initial comit

---
+++ src/main/java/com/rifqi/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.rifqi.springboot.cruddemo.service;
+
+import com.rifqi.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> findAll();
+
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int theId);
+}
