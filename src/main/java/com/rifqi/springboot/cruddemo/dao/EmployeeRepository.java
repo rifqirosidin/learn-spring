83cf9f43199353d989d032d5247baed1b1709165
Rifqi Rosidin <rifqi.rosidin@gmail.com>
2021-01-16 22:10:33 +0700

    crud with Spring data rest

--- src/main/java/com/rifqi/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/rifqi/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.rifqi.springboot.cruddemo.dao;
 
 import com.rifqi.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
